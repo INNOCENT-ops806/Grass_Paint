5c25a77ef07f1e980df1a269758604333e6d387f
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-07-06 21:31:12 +0200

    add FlatLaf look and Feel

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 public class App {
   public static void main(String[] args) {
+    try {
+      FlatLightLaf.setup();
+    } catch (Exception e) {
+      System.err.println("Error: " + e.getMessage());
+    }
     new MyFrame();
   }
 }
