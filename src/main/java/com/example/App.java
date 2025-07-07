c9a714f3340c3918ffd060d928081a8d01b0fa33
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-07-07 19:28:34 +0200

    use SwingUtilities for EDT(Event Dispatch Thread) safety

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,14 +1,22 @@
 package com.example;
 
+import javax.swing.SwingUtilities;
+
 import com.formdev.flatlaf.FlatLightLaf;
 
 public class App {
   public static void main(String[] args) {
-    try {
-      FlatLightLaf.setup();
-    } catch (Exception e) {
-      System.err.println("Error: " + e.getMessage());
-    }
-    new MyFrame();
+    SwingUtilities.invokeLater(new Runnable() {
+
+      @Override
+      public void run() {
+        try {
+          FlatLightLaf.setup();
+        } catch (Exception e) {
+          System.err.println("Error: " + e.getMessage());
+        }
+        new MyFrame();
+      }
+    });
   }
 }
